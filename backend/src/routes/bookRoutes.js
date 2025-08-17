0d60a833727dc4c51b0fa616cf06bc14cda8c591
om salunke <omsaunke343@gmail.com>
2025-08-17 20:30:34 +0530

    book api implmentation done

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import Book from "../models/Book";
 import cloudinary from "../lib/cloudinary";
-import protectRoute from "../middleware/auth.middleware.js"
+import protectRoute from "../middleware/auth.middleware.js";
 const router = express.Router();
 
-router.post("/", protectRoute,async (req, res) => {
+router.post("/", protectRoute, async (req, res) => {
   try {
     const { title, caption, rating, image } = req.body;
     if (!image || !title || !caption || !rating)
@@ -18,7 +18,7 @@ router.post("/", protectRoute,async (req, res) => {
       caption,
       rating,
       image: imageUrl,
-    //   user: req.user._id,
+      user: req.user._id,
     });
 
     await newBook.save();
@@ -26,9 +26,72 @@ router.post("/", protectRoute,async (req, res) => {
     res.status(201).json(newBook);
     //save to database
   } catch (error) {
-    res.status(500).json({message:error.message})
+    res.status(500).json({ message: error.message });
   }
 });
-//Om he about page ch ani last page ch  animation reload kelay var yetay tar te scroll kelay var direct yayla pahije
+
+//get all books
+//pagination
+router.get("/", protectRoute, async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const skip = (page - 1) * limit;
+    const books = await Book.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate("user", "username profileImage");
+    const total = await Book.countDocuments();
+    res.send({
+      books,
+      currentPage: page,
+      totalBooks: total,
+      totalPage: Math.ceil(total / limit),
+    });
+    //save to database
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.delete("/:id", protectRoute, async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) return res.status(404).json({ message: "Book not found" });
+
+    //check if user is creator of the book
+
+    if (book.user.toString() != req.user._id.toString()) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    //delete image from cloudinary
+    if ((book.image && book.image.includes("cloudinary"))) {
+      try {
+        const publicId = book.image.split("/").pop().split(".")[0];
+        await cloudinary.uploader.destroy(publicId);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    await book.deleteOne();
+    res.json({ message: "Book deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+router.get("/user",protectRoute,async(req,res)=>{
+  try {
+    const books = await Book.find({user:req.user._id}).sort({createdAt:-1});
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+
+  }
+})
+
 
 export default router;
